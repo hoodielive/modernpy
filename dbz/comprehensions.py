7966fddd0dfa26799a743c1e5133dbb58ae1cd76
hoodielive <hoodielive@gmail.com>
2019-07-17 23:54:38 -0400

    lawrdie, this is it for tonight

--- dbz/comprehensions.py
+++ dbz/comprehensions.py
@@ -14,3 +14,12 @@ for x in range(5):
     for y in range(3):
         points.append((x,y))
 print(points)
+
+values = [ x / (x-y) for x in range(100) if x > 50 for y in range(100) if x - y != 0 ]
+
+print(values)
+
+m = map(ord, 'turtles all the way down')
+
+print(m)
+print(isinstance(m, map))
