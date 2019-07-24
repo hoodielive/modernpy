47c65a089da8b939cc16b5a34df5ff8f59566d28
hoodielive <hoodielive@gmail.com>
2019-07-24 18:06:17 -0400

    more comprehensions

---
+++ matrix.py
@@ -0,0 +1,22 @@
+my_lists = [ 
+        [[1, 2, 3], [4,5,6], [7,8,9]] 
+]
+
+flat = [x for sublist1 in my_lists
+        for sublist2 in sublist1
+        for x in sublist2
+]
+
+print(flat)
+
+a = [1,2,3,4,5,6,7,8,9,10] 
+b = [ x for x in a if x > 4 if x % 2 == 0 ] 
+c = [ x for x in a if x > 4 and x % 2 == 0 ] 
+
+print(b) 
+print(c) 
+
+matrix = [[1,2,3], [4, 5, 6], [7,8,9]] 
+filtered = [[ x for x in row if x % 3 == 0 ] 
+            for row in matrix if sum(row) >= 10 ] 
+print(filtered) 
