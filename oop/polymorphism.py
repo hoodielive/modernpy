15893a17aabd872afc9f4434a65ddd065c5cf339
hoodielive <hoodielive@gmail.com>
2019-10-05 13:52:16 -0400

    Polymorphism - hi Larry

---
+++ oop/polymorphism.py
@@ -0,0 +1,13 @@
+# Polymorphism is method overloading and method overriding 
+# Its also operator overloading 
+
+class Man: 
+    def sayHi(self, name=None):
+        if name is not None: 
+            print("Hi " + name)
+        else:
+            print("Hi!")
+        
+
+obj = Man() 
+obj.sayHi("Larry")
