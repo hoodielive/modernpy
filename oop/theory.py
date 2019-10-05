d381f86f2dbd4288b71707bdc4370b451f58a200
hoodielive <hoodielive@gmail.com>
2019-10-05 12:43:36 -0400

    volvo

--- oop/theory.py
+++ oop/theory.py
@@ -1,14 +1,16 @@
 class Car: 
+    car_type = "suv"
+    model = "S90"
+    mileage = 6
+
     def start(self):
         print("Car started!")
 
     def reverse(self): 
         print("Car taking a reverse")
 
-def main():
-    volvo = Car()
-    volvo.start() 
-    volvo.reverse() 
 
-if __name__ == "__main__":
-    main()
+new_car = Car() 
+    print(new_car.car_type)
+    print(new_car.model)
+    print(new_car.mileage)
