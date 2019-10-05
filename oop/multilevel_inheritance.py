0602148d689c1ed8f2ca38204c8b17e7de7aca16
hoodielive <hoodielive@gmail.com>
2019-10-05 13:28:09 -0400

    Multilevel Inheritance

---
+++ oop/multilevel_inheritance.py
@@ -0,0 +1,14 @@
+class Father(object):
+    def __init__(self):
+        super(Father, self).__init__()
+        print("Father")
+
+class Mother(object):
+    def __init__(self):
+        super(Mother, self).__init__()
+        print("Mother")
+
+class Son(Mother, Father): 
+    def __init__(self):
+        super(Son, self).__init__()
+        print("Son")
