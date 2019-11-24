254310a434764b6540502e4ab08ff24b1991e5ea
hoodielive <hoodielive@gmail.com>
2019-11-24 14:42:56 -0500

    requests is more like it because it abstracts away all the other bullshit

--- RequestsPlay/index.py
+++ RequestsPlay/index.py
@@ -1,6 +1,6 @@
-import urllib.request 
-import urllib.parse 
+from pprint import pprint
+import requests 
 
-url = 'https://api.spotify.com/v1/searc?type=artist&q=snoop'
-f = urllib.request.urlopen(url) 
-print(f.read().decode('utf-8'))
+r = requests.get('https://api.spotify.com/v1/search?type=artist&q=snoop')
+pprint(r.json())
+
