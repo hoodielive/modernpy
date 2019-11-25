93242ac0cbb8750f2f35358a7eff3dce9453adbe
hoodielive <hoodielive@gmail.com>
2019-11-25 15:33:12 -0500

    Python needs some love too

--- pyworkshop/namelib.py
+++ pyworkshop/namelib.py
@@ -1,9 +1,14 @@
 def upper_case_name(name):
     return name.upper() 
 
-
 name = "Larry"
+
 upper_name = upper_case_name(name)
 
 print(f"Upper case name is {upper_name}"); 
-print("dunder name is ", __name__)
+
+
+if __name__ == "__main__":
+    name = "Larry"
+    upper_name  = upper_case_name(name)
+    print(f"dunder name", __name__)
