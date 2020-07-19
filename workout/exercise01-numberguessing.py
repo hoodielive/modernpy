91f8cd7dc0cd2f4cc1958bdec7540de0e41d7f95
hoodielive <hoodielive@gmail.com>
2020-07-19 02:14:59 -0400

    Python workout exercise 01 - Number guessing.

---
+++ workout/exercise01-numberguessing.py
@@ -0,0 +1,19 @@
+from random import randint as rand
+
+# dry run. cheat
+
+def guessing_game():
+    answer = rand(0, 100)
+
+    while True: 
+        user_guess = int(input('What is your guess? '))
+        if user_guess == answer:
+            print(f'Right! The answer is {user_guess} ')
+            break
+        if user_guess < answer: 
+            print(f'Your guess of {user_guess} is to low! ')
+        else:
+            print(f'Your guess of {user_guess} is too high! ')
+
+guessing_game()
+                               
