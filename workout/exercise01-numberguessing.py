ad19f109d525de6cb01785811954a25ce357c06d
hoodielive <hoodielive@gmail.com>
2020-07-19 02:16:19 -0400

    Removing those god awful trailing spaces - git log -p -2

--- workout/exercise01-numberguessing.py
+++ workout/exercise01-numberguessing.py
@@ -1,19 +1,18 @@
 from random import randint as rand
 
-# dry run. cheat
+# Dry run. cheat
 
 def guessing_game():
     answer = rand(0, 100)
 
-    while True: 
+    while True:
         user_guess = int(input('What is your guess? '))
         if user_guess == answer:
             print(f'Right! The answer is {user_guess} ')
             break
-        if user_guess < answer: 
+        if user_guess < answer:
             print(f'Your guess of {user_guess} is to low! ')
         else:
             print(f'Your guess of {user_guess} is too high! ')
 
 guessing_game()
-                               
