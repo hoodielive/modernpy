44bf56786a42a4124e006cffa1e850ee5545934f
hoodielive <hoodielive@gmail.com>
2020-08-03 22:31:52 -0400

    Practice Python for classes.

---
+++ prepare/review_data_structures.py
@@ -0,0 +1,23 @@
+# Lists.
+
+todo = [ "pick up here", "buy groceries", "pay bills" ]
+
+for i in todo:
+    print(i)
+
+# Dictionaries.
+
+User = { 
+    "name" : "Jeriah", 
+    "age": 27, 
+    "email" : "someemail@email.com"
+}
+
+# Loop over Dictionary.
+for i in User.items():
+    print(i)
+
+tuples = ("i", "am", "a", "tuple", "of", "strings")
+
+for i in tuples:
+    print(i)
