b69789a8c1d49b6f9ce18793fc1539a93f901a9a
hoodielive <hoodielive@gmail.com>
2020-08-04 21:51:20 -0400

    Preparation for Python.

--- prepare/review_data_structures.py
+++ prepare/review_data_structures.py
@@ -2,6 +2,14 @@
 
 todo = [ "pick up here", "buy groceries", "pay bills" ]
 
+X = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+Y = [[10, 11, 12], [13, 14, 15], [16, 17, 18]]
+
+result = [[0, 0, 0],[0, 0, 0], [0, 0, 0]]
+
+for printed_result in X, Y, result: 
+    print(X, Y, result)
+
 for i in todo:
     print(i)
 
