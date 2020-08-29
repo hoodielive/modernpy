d7ded343b9699b4b2b782b2bf7d6bcdb4d9945ff
hoodielive <hoodielive@gmail.com>
2020-08-28 21:00:49 -0400

    This is a script to group things. Pretty neat.

---
+++ grouping_in_python.py
@@ -0,0 +1,10 @@
+from itertools import groupby
+items = [(1, "a"), (3, "q"), (2, "c"), (2, "x"), (1, "z")]
+
+for key, group in groupby(sorted(items), lambda x: x[0]):
+    print(key)
+    for tuple in group:
+        print(tuple)
+    print("------------------")
+
+
