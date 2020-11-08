3725bb234d89276a151e29a7f4d72d7cbd3be631
hoodielive <hoodielive@gmail.com>
2020-11-07 21:04:08 -0500

    Tuple packing and unpacking.

---
+++ expert/tuple_packing.py
@@ -0,0 +1,10 @@
+# Tuple packing.
+
+food = ('orange', 'apples', 'lemons', 'limes')
+
+first_fruit, second_fruit, third_fruit, fourth_fruit = food
+
+print(first_fruit)
+print(second_fruit)
+print(third_fruit)
+print(fourth_fruit)
