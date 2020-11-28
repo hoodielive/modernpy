e8af8b2a4789ddc8fe6c375bb85bb7dda3a4ca8e
hoodielive <hoodielive@gmail.com>
2020-11-27 23:05:58 -0500

    find me

--- vs/chipper.py
+++ vs/chipper.py
@@ -1,5 +1,9 @@
 student_tuple = ()
 
+time_tuple = (9, 16, 1)
+
+print(time_tuple[0] * 3600 / time_tuple[1] * 60 + time_tuple[2])
+
 print(len(student_tuple))
 
 student_tuple = "hang", "in", "the", "balance"
