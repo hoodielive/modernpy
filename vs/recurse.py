ed10f0a5458d66026b974d9b792b9ad2bf1271e3
hoodielive <hoodielive@gmail.com>
2020-12-08 20:42:02 -0500

    Solve for factorial

---
+++ vs/recurse.py
@@ -0,0 +1,10 @@
+def factorial(x):
+    """ This is the recursive function to find the factorial of an integer. """
+
+    if x == 1:
+        return 1
+    else:
+        return (x * factorial(x-1))
+
+num = 5
+print("The factorial of", num, "is", factorial(num))
