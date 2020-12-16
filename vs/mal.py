bcc084f898b979317ab50a50b74f96ae66a2ef5a
hoodielive <hoodielive@gmail.com>
2020-12-15 23:27:37 -0500

    This is a secret.

---
+++ vs/mal.py
@@ -0,0 +1,11 @@
+SECRET = 'This is a secret.'
+
+class Error:
+    def __init__(self):
+        pass
+
+err = Error()
+user_input = '{error.__init__.__globals__[SECRET]}'
+
+print(user_input.format(error=err))
+
