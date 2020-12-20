51d1dafbae4cae2096b62cdc9d7fbfd68165b5fc
hoodielive <hoodielive@gmail.com>
2020-12-20 11:19:05 -0500

    Work on midterm

---
+++ vs/midTest.py
@@ -0,0 +1,54 @@
+# You have to 5:30pm to complete the following Test.
+# Start now. 
+
+# Item 1
+# Write a class that Models a Human. 
+# The class should depict the head, shoulders (left and right), legs (left and right), reproductive organs (fe/male)
+# As a mininum, there should be 3 methods that return body parts, sets body parts and returns a message about the class. 
+# Instantiate the class with the variable naming convention of lst_instantiation, 2nd and so forth - there should be 3 instantiations. 
+
+
+# Item 2
+# Write a base class with a few properties for a class that will inherit from it. 
+
+# Item 3 What is duck typing? 
+
+# Write 4 try and except statements on Syntax Error, Value Error, Type Error, Attribute Error
+
+# Write 5 functions depicting and returning *args and **kwargs 
+
+# Write 2 functions that returns and integer to another function.
+
+# Write 2 functions that have default arguments.
+
+# Write 3 lambda functions. 
+
+# Use the filter function to filter from a list that contains the integer 5 (should be excluded)
+
+# What is a Stack? 
+
+# What is a queue? 
+
+# Write 3 list comprehensions. 
+
+# Write 3 for-loops over a list. 
+
+# Write 3 for-loops over a dictionary. 
+
+# Write 3 for-loops over a Tuple.
+
+# Write a for-loop over a string. 
+
+# What is the difference between an Array and a List? 
+
+# Write 3 dictionary comprehensions. 
+
+# Unpack 2 tuples.
+
+# What does the dir function do? 
+
+# Explain the Module search path. 
+
+
+
+
