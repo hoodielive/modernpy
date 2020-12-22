ef4d5b64119c8d699eac15100fa3fefefd0002a7
hoodielive <hoodielive@gmail.com>
2020-12-22 11:13:25 -0500

    Um.. cough.

--- vs/chipper.py
+++ vs/chipper.py
@@ -1,4 +1,5 @@
 student_tuple = ()
+
 student_tuple += ('Vince', 'Jordan')
 
 student_tuple += ('Basically', 'Complexity')
@@ -6,6 +7,7 @@ student_tuple += ('Basically', 'Complexity')
 print(student_tuple)
 
 time_tuple = (9, 16, 1)
+
 print(time_tuple)
 
 print(time_tuple[0] * 3600 / time_tuple[1] * 60 + time_tuple[2])
