65dad9422550826b5c0df6a6c8cef77578bb8d22
hoodielive <hoodielive@gmail.com>
2020-12-23 21:55:15 -0500

    Decorators are a way to clean up code. So refreshing.

--- vs/Decor.py
+++ vs/Decor.py
@@ -25,19 +25,22 @@ def register(f):
 def foo(): return 'bar'
 
 def check_is_admin(username):
-    if username != 'admin':
-        raise Exception("This user is not allowed to get food.")
+    def wrapper(*args, **kwargs):
+        if kwargs.get('username') != 'admin':
+            raise Exception("This user is not allowed to get food.")
+        return f(*args, **kwargs)
+    return wrapper
 
 class Store(object):
     def __init__(self, storage):
         self._storage = storage
 
+    @check_is_admin
     def get_food(self, username, food):
-        check_is_admin(username)
         return self._storage.get(food)
 
+    @check_is_admin
     def put_food(self, username, food):
-        check_is_admin(username)
         self._storage.put(food)
     
 
