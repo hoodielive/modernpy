5a0206abba65ffaeecd76cbf4c8a4b912a5fbc18
hoodielive <hoodielive@gmail.com>
2020-12-26 11:14:05 -0500

    Teaching Brandon about Modules.

---
+++ vs/mosh/sales.py
@@ -0,0 +1,8 @@
+def calc_tax():
+    """This is a simple function that talks noise"""
+    return "This is going to be expensive."
+
+def calc_shipping():
+    """This is the second simple function that talks noise"""
+    return  "I AM the primordial G"
+
