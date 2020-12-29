5abb74a3f1cb32d314b112c72675206473cc690c
hoodielive <hoodielive@gmail.com>
2020-12-28 19:33:10 -0500

    Instructing examples.

---
+++ vs/learn.py
@@ -0,0 +1,8 @@
+
+tup = (1, 2, 3, 4)
+
+tup_list = list(tup)
+
+tup_reversed = tup_list.reverse()
+
+print(tup_reversed)
---
+++ vs/reverse_a_tuple.py
@@ -0,0 +1,14 @@
+# Construct Tuple.
+
+tup = (22, 65, 12, 54)
+
+# Cast tup into a list.
+
+tup_list = list(tup)
+print(tup_list)
+
+# Reverse the list. 
+
+tup_list_reversed = tup_list.reverse()
+
+print(tup_list_reversed)
