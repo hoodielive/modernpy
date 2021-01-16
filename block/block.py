d8ff4d9e29be0a54fc1a75578cf0bfce48664716
hoodielive <hoodielive@gmail.com>
2021-01-16 11:36:04 -0500

    Block chain py.

---
+++ block/block.py
@@ -0,0 +1,59 @@
+# A blockchain distributed data storage consisting of containers 
+# (blocks) which are connected. If the data you store in a block 
+# is a list of transactions, the coins transferred in the transactions 
+# form your cryptocurrency.
+
+print("Data Storage.")
+
+# A single block can hold any type of data you want.
+# Chain-> each folder/container/block knows about the 
+# folder/container/block before it.
+# The whole folder is the blockchain respectively.
+
+print("Blockchain defined.")
+
+# Block chains are not limited to transactions.
+# A blockchain is a data store.
+# Bitcoin blockchain is a 'list of transactions'. 
+# Yes, they know about each other.
+# Each block is hashed that is generated from all the parts
+# in the block.
+
+print("Cryptography - Hash Algorithms.")
+
+# If you change some input, the output will also change.
+# Meaning, a new hash will be generated.
+
+print("What are coins?")
+
+# Coins are transferred with Transactions.
+# Changing them into other currencies is not possible inside
+# of a blockchain. The value of a coin is
+# determined by the value the participants of the
+# blockchain network assign to i (i.e. what you're willing to
+# pay in 'fiat'..sigh currencies). Coins are 'created' via Mining 
+# (as a reward for 'effort').
+
+print("Coins are distributed to Nodes called 'Wallets'")
+
+# A wallet is a node.
+# You can send coins through transactions to other wallets.
+# Whenever a transaction is created its broadcasted to the nodes.
+# The nodes are distributed over the node network.
+# They first will verify your identity.
+# The transaction is then 'bundled' and stored in a newly generated
+# (minded) block.
+
+print("So the base syntax is: ")
+
+# Base Syntax: Basic Blockchain -> List of Coins.
+# Loops & Conditionals: Blockchain with Chain Validation.
+# Working with Complex Data Structures: Store real blocks and transactions.
+# More Functions -> Calculate & Output balances.
+# Using Standardard Library - Create real Hashes/Proof of work.
+# Reading and Writing Files - Store and Load Blockchain.
+# Error Handling - Make Blockchain more Robust (reinforce blockchain).
+# Object Oriented Programming: Add a wallet.
+# Modules and Packages: Sign Transactions.
+# Handling HTTP requests with Flask: Control Blockchain via Web UI.
+# HTTP Communication between PyPrograms: Connect Blockchain Nodes.
