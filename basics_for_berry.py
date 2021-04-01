de34e5966b849060af378cb741576a5a01c46c1e
hoodielive <hoodielive@gmail.com>
2021-04-01 00:13:58 -0400

    Lolita wanted to see some Python -- booom.

---
+++ basics_for_berry.py
@@ -0,0 +1,11 @@
+# Strings, characters.
+# Booleans, False or True.
+# Integers, numbers.
+# Floats, numbers that have a decimal point.
+
+c = 0 == False
+
+if c == True:
+    print("It is true")
+else:
+    print("It is NOT True")
