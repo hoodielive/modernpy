7579b399ac3696d35aa47ff29fdf6af04e838be9
hoodielive <hoodielive@gmail.com>
2023-10-11 22:43:53 -0400

    This is better.

---
+++ Odu/_main5.py
@@ -0,0 +1,39 @@
+import random
+
+# Function to load statements from the file
+def load_statements(file_name):
+    with open('odus.txt', 'r', encoding='utf-8') as file:
+        statements = file.readlines()
+    return statements
+
+# Function to start the quiz
+def start_quiz(statements):
+    random.shuffle(statements)
+    correct_answers = 0
+
+    for statement in statements:
+        # Split the statement into parts based on the period and a space
+        parts = statement.strip().split('. ')
+        if len(parts) != 2:
+            continue
+
+        # Extract the number and statement text
+        number, statement_text = parts
+
+        # Extract the word right after the number and period
+        statement_word = statement_text.split(' ', 1)[0]
+
+        user_answer = input(f"\nStatement:\n{number}. [Hidden] {statement_text}\nEnter the correct word (Odu): ")
+
+        if user_answer.strip() == statement_word:
+            print("Correct!")
+            correct_answers += 1
+        else:
+            print(f"Wrong. The correct word (Odu) is: {statement_word}")
+
+    print(f"Quiz completed! You got {correct_answers} out of {len(statements)} words correct.")
+
+if __name__ == "__main__":
+    statements = load_statements("odus.txt")
+    start_quiz(statements)
+
---
+++ Odu/_main8.py
@@ -0,0 +1,42 @@
+import random
+
+# Function to load statements from the file
+def load_statements(file_name):
+    with open('odus.txt', 'r', encoding='utf-8') as file:
+        statements = file.readlines()
+    return statements
+
+# Function to start the quiz
+def start_quiz(statements):
+    random.shuffle(statements)
+    correct_answers = 0
+
+    for statement in statements:
+        # Split the statement into parts based on the period and a space
+        parts = statement.strip().split('. ')
+        if len(parts) != 2:
+            continue
+
+        # Extract the number and statement text
+        number, statement_text = parts
+
+        # Extract the word right after the number and period
+        statement_word = statement_text.split(' ', 1)[0]
+
+        # Replace the word with [Hidden]
+        hidden_statement = statement_text.replace(statement_word, "[Hidden]", 1)
+
+        user_answer = input(f"\nStatement:\n{number}. {hidden_statement}\nEnter the correct word (Odu): ")
+
+        if user_answer.strip() == statement_word:
+            print("Correct!")
+            correct_answers += 1
+        else:
+            print("Wrong. The correct word (Odu) is hidden.")
+
+    print(f"Quiz completed! You got {correct_answers} out of {len(statements)} words correct.")
+
+if __name__ == "__main__":
+    statements = load_statements("odus.txt")
+    start_quiz(statements)
+
