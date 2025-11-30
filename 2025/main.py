a185be00c505fc997150b4a453bc8ea08f2a8037
hoodielive <hoodielive@gmail.com>
2025-11-29 21:37:26 -0500

    Program / Main

---
+++ 2025/main.py
@@ -0,0 +1,26 @@
+class employee: 
+    __name = "" # an attribute
+ 
+    # constructor
+    def __init__(self, name):
+        self.__name = name
+
+    def demo_class():
+    # Create employee object
+        e = employee('James')
+
+    def get_name(self):
+        return self.__name.upper()
+
+    def set_name(self):
+        self.__name = value + "(Name changed.)"
+
+    # Write employee name
+    def getIntro(self):
+        return "Name of employee is {}.".format(self.get_name())
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
